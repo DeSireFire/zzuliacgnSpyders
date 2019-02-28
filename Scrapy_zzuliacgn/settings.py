998eeb30ae306313baa4034afb67e80431d57b18
DeSireFire <1025212779@qq.com>
2019-03-01 00:30:34 +0800

    初次更新。

---
+++ Scrapy_zzuliacgn/settings.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from Scrapy_zzuliacgn.tools import config
+import random
+# Scrapy settings for Scrapy_zzuliacgn project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'Scrapy_zzuliacgn'
+
+SPIDER_MODULES = ['Scrapy_zzuliacgn.spiders']
+NEWSPIDER_MODULE = 'Scrapy_zzuliacgn.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# USER_AGENT = 'Scrapy_zzuliacgn (+http://www.yourdomain.com)'
+USER_AGENT = random.choice(config.USER_AGENTS)
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = config.get_header()
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+# SPIDER_MIDDLEWARES = {
+#    'Scrapy_zzuliacgn.middlewares.ScrapyBtSpiderMiddleware': 543,
+# }
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+    'Scrapy_zzuliacgn.middlewares.RandomUserAgentMiddleware': 543,
+}
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   'Scrapy_zzuliacgn.pipelines.mysqlPipeline': 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# 修改编码为utf-8
+# FEED_EXPORT_ENCODING = 'utf-8'
+
+# 数据库信息
+# MYSQL配置
+# 默认
+MYSQL_HOST = '192.168.0.107' # 数据库地址
+MYSQL_DATABASE = 'zzuli_ACGN' # 数据库名
+MYSQL_USER = 'zzuliACGN'
+MYSQL_PASSWORD = 'DeSireFire233notRoot'
+MYSQL_PORT = 3306
+
+
+# 代理设置
+PROXY_URL = 'http://localhost:5555/random'
