4bfe2eea93e04cb6de348201099b4872080e5f73
DeSireFire <1025212779@qq.com>
2019-06-12 23:52:27 +0800

    本日最后一次更新

--- Scrapy_zzuliacgn/spiders/wenku8Net.py
+++ Scrapy_zzuliacgn/spiders/wenku8Net.py
@@ -5,7 +5,7 @@ import scrapy
 class Wenku8netSpider(scrapy.Spider):
     name = "wenku8Net"
     allowed_domains = ["wenku8.net", "wkcdn.com"]
-    start_urls = ['https://www.wenku8.net/book/1.htm']
+    start_urls = ['https://www.wenku8.net/book/5000.htm']
 
     # xpath 字典
     xpathDict = {
@@ -19,7 +19,16 @@ class Wenku8netSpider(scrapy.Spider):
         '简介':"//span[5]/text()",
     }
     def parse(self, response):
-        firstDict = {}
+        firstDict = {
+            '书名':"//table[1]//span//b/text()",
+            '封面':"//td//img[@vspace='0']/@src",
+            '文库分类':"//div[@id='content']/div[1]//tr[2]/td[1]/text()",
+            '作者名':"//div[@id='content']/div[1]//tr[2]/td[2]/text()",
+            '文章状态':"//div[@id='content']/div[1]//tr[2]/td[3]/text()",
+            '最后更新':"//div[@id='content']/div[1]//tr[2]/td[4]/text()",
+            '全文字数':"//div[@id='content']/div[1]//tr[2]/td[5]/text()",
+            '简介':"//span[5]/text()",
+        }
         for k,v in self.xpathDict.items():
             firstDict[k] = self.xpathHandler(response,v)
         print(firstDict)
@@ -32,10 +41,10 @@ class Wenku8netSpider(scrapy.Spider):
         :return:
         '''
         temp = response.xpath(xpathStr).extract()
-        if len(temp) == 1:
+        if temp:
             return temp[0]
-        return temp
-
+        else:
+            return ['暂无数据'][0]
 
 if __name__ == '__main__':
     from scrapy import cmdline
